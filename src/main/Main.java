a4d8d5addadb7d48ce45dfd3b74e20b64d1b7517
ajbinky <ajbinky@gmail.com>
2017-11-22 11:58:58 -0600

    Removed stuff in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,7 @@
-import time.Timesheet;
-
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		Timesheet ts = new Timesheet();
-		ts.clockIn();
-		Thread.sleep(5000);
-		ts.clockOut();
-		ts.clockIn();
-		Thread.sleep(5000);
-		ts.clockOut();
-		ts.clockIn();
-		Thread.sleep(5000);
-		ts.clockOut();
-		ts.clockIn();
-		Thread.sleep(5000);
-		ts.clockOut();
-		System.out.println(ts.toString());
-		System.out.println("Hello World!");
+		
 	}
 
 }
