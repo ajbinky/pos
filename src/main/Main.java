841f18c3d623e97619a6841d3bd5119cb30a6828
ajbinky <ajbinky@gmail.com>
2017-12-11 13:12:31 -0600

    Main.userInterface()

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,42 +3,66 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		String menuString = userInterface("Reservations", "Menu/Order", "Employee Time Sheets", "Product List", "Exit");
 		createResFile createResFileObject = new createResFile();
 		int menu = 0;
 		Scanner in = new Scanner(System.in);
-		while(true) {
-		System.out.println("****************************");
-		System.out.println("Welcome!");
-		System.out.println("Please select one of the menu items using its number value");
-		System.out.println("1: Reservations");
-		System.out.println("2: Menu/Order");
-		System.out.println("3: Employee Time Sheets");
-		System.out.println("4: Product List");
-		System.out.println("5: Exit");
-		System.out.println("****************************");
-		
-		menu = in.nextInt();
-		
-		
-		switch (menu) {
-			case 1:  createResFileObject.reservation(in);
-					 break;
-			case 2:  System.out.println("This is a placeholder! This is for the Menu");
-					 break;
-			case 3:  ;
-					 break;
-			case 4:  System.out.println("This is a placeholder! This is for the Product list");
-					 break;
-			case 5:  
+		while (true) {
+			System.out.println(menuString);
+
+			menu = in.nextInt();
+
+			switch (menu) {
+			case 1:
+				createResFileObject.reservation(in);
+				break;
+			case 2:
+				System.out.println("This is a placeholder! This is for the Menu");
+				break;
+			case 3:
+				;
+				break;
+			case 4:
+				System.out.println("This is a placeholder! This is for the Product list");
+				break;
+			case 5:
 				in.close();
 				System.exit(0);
-			default: System.out.println("We're sorry. We don't recognize that value");
-					 break;
-		}
+			default:
+				System.out.println("We're sorry. We don't recognize that value");
+				break;
+			}
 		}
 
 	}
 	
-
+	/**
+	 * 
+	 * @param args
+	 * 		Strings that you want as options in the menu
+	 * @return String
+	 * 		String built with the given arguments
+	 * 
+	 * for example
+	 * Main.userInterface("Hello world!");
+	 * would return a string that, when printed, would look like this:
+	 * ****************************
+	 * Welcome!
+	 * Please select one of the menu items using its number value
+	 * 1: Hello world!
+	 * ****************************
+	 */
+	public static String userInterface(String... args) {
+		int counter = 1;
+		StringBuilder sb = new StringBuilder();
+		sb.append("****************************\n");
+		sb.append("Welcome!\n");
+		sb.append("Please select one of the menu items using its number value\n");
+		for (String s : args) {
+			sb.append(counter++ + ": " + s + "\n");
+		}
+		sb.append("****************************\n");
+		return sb.toString();
+	}
 
 }
